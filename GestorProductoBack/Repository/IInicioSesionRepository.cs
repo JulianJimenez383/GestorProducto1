5a0126b1e8f194beedc96704e526c4466b786d5e
Julian <brsju@DAVID>
2024-11-18 21:55:03 -0500

    Se ingresa imagen de prueba, ce crea contoller para inicio sesion, se modifican campos de registro login y de ingresar

---
+++ GestorProductoBack/Repository/IInicioSesionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GestorProductoBack.Repository
+{
+    public interface IInicioSesionRepository<T> : IDisposable where T : class
+    {
+        Task<T> GetById(string id);
+
+
+
+    }
+}
---
+++ GestorProductoBack/Repository/InicioSesionRepository.cs
@@ -0,0 +1,36 @@
+﻿using GestorProductoBack.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace GestorProductoBack.Repository
+{
+
+    public class InicioSesionRepository<T> : IInicioSesionRepository<T> where T : class
+    {
+        private InventarioDesarrolloWebEntities _dbContex = new InventarioDesarrolloWebEntities();
+
+        private DbSet<T> dbSet
+        {
+            get
+            {
+                return _dbContex.Set<T>();
+            }
+        }
+
+        public async Task<T> GetById(string id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
