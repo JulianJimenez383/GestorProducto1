af41cacdb1ef15a8612d6c0e878dbc5f27962e10
Julian <brsju@DAVID>
2024-11-19 18:50:02 -0500

    se envian cambios

--- GestorProducto1/Controllers/LoginController.cs
+++ GestorProducto1/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GestorProductoBack.Model;
+using GestorProductoBack.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,24 @@ namespace GestorProducto1.Controllers
 {
     public class LoginController : Controller
     {
+
+        private InventarioDesarrolloWebEntities db = new InventarioDesarrolloWebEntities();
+        GestorRepository<Usuario> data = new GestorRepository<Usuario>();
         // GET: Login
-        public ActionResult Index()
+        public ActionResult Index(string usu , string contra)
         {
+
+            var vista = new Usuario();
+
+            if (usu == null || contra == null) { 
+                return View(vista); 
+            
+            
+            }
+
+
+
+
             return View();
         }
     }
