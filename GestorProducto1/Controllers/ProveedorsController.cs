2f2ae0ef4f639cd78e0a10f7e072b30ea0e8682f
Julian <brsju@DAVID>
2024-11-20 20:59:06 -0500

    Se modifica vistas y controladores para mostrar la informacion adecuada, se procede a implementar controlador de movimientos de bodegas y movimientos de usuarios

---
+++ GestorProducto1/Controllers/ProveedorsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GestorProductoBack.Model;
+
+namespace GestorProducto1.Controllers
+{
+    public class ProveedorsController : Controller
+    {
+        private InventarioDesarrolloWebEntities db = new InventarioDesarrolloWebEntities();
+
+        // GET: Proveedors
+        public ActionResult Index()
+        {
+            var proveedor = db.Proveedor.Include(p => p.Usuario);
+            return View(proveedor.ToList());
+        }
+
+        // GET: Proveedors/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "IdUsuario");
+            return View();
+        }
+
+        // POST: Proveedors/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdProveedor,NombreProveedor,DireccionProveedor,TelefonoProveedor,CorreoProveedor,IdUsuario")] Proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Proveedor.Add(proveedor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "Password", proveedor.IdUsuario);
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "Password", proveedor.IdUsuario);
+            return View(proveedor);
+        }
+
+        // POST: Proveedors/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdProveedor,NombreProveedor,DireccionProveedor,TelefonoProveedor,CorreoProveedor,IdUsuario")] Proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proveedor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdUsuario = new SelectList(db.Usuario, "IdUsuario", "Password", proveedor.IdUsuario);
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proveedor);
+        }
+
+        // POST: Proveedors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Proveedor proveedor = db.Proveedor.Find(id);
+            db.Proveedor.Remove(proveedor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
